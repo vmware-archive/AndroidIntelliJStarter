04471a36094c07d8b4610be20933f514d67001d8
Joe Moore <pair+joe@pivotallabs.com>
2011-08-03 10:10:42 -0700

    Added Google guava, httpmime, jackson, great-expectations. Added sample test using Great Expectations.

---
+++ test/java/com/example/android/sampleapp/StarterActivityTestWithGreatExpectations.java
@@ -0,0 +1,21 @@
+package com.example.android.sampleapp;
+
+import android.widget.TextView;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.example.android.sampleapp.test.support.Expect.expect;
+
+@RunWith(RobolectricTestRunner.class)
+public class StarterActivityTestWithGreatExpectations {
+    @Test
+    public void shouldHaveATitle() {
+        final StarterActivity activity = new StarterActivity();
+        activity.onCreate(null);
+        TextView title = (TextView) activity.findViewById(R.id.title);
+        expect(title).not.toBeNull();
+        expect(title.getText()).toBeInstanceOf(String.class);
+        expect(title.getText()).toEqual("Hello World");
+    }
+}
