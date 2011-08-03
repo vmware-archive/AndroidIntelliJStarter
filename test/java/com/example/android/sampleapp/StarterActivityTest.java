3225f178958a28b6e4194cf46cd30986e1d20aca
David Farber & Joe Moore <pair+dfarber+joe@pivotallabs.com>
2011-08-03 07:12:35 -0600

    WIP

--- test/java/com/example/android/sampleapp/StarterActivityTest.java
+++ test/java/com/example/android/sampleapp/StarterActivityTest.java
@@ -2,18 +2,42 @@ package com.example.android.sampleapp;
 
 import android.widget.TextView;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
+//@RunWith(Enclosed.class)
 public class StarterActivityTest {
+
+    @Before
+    public void setup() {
+        System.out.println("Base Before.");
+    }
+
     @Test
     public void shouldHaveATitle() {
+        System.out.println("Base test case.");
         final StarterActivity activity = new StarterActivity();
         activity.onCreate(null);
         TextView title = (TextView) activity.findViewById(R.id.title);
         assertEquals("Hello World", title.getText());
+    }
+
+    public static class NestedTestClass {
+
+        @Before
+        public void setup() {
+            System.out.println("Inner Before.");
+        }
+
+        @Test
+        public void shouldRunNestedTestCase() throws Exception {
+            System.out.println("Nested test case.");
+            assertTrue(true);
+        }
     }
 }
