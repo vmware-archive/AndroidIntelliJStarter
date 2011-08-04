e8c96b0692d168318eeb3bd64c3ccd0d8e6d530f
Joe Moore <pair+joe@pivotallabs.com>
2011-08-04 15:22:38 -0700

    project rename script WIP

--- script/helpers.rb
+++ script/helpers.rb
@@ -2,3 +2,20 @@ def bail_with(project)
     puts ">>>> #{project} failed! <<<<"
     exit(1)
 end
+
+
+def rename_to(name)
+  Dir.glob('.idea/**/*.xml').each do |filename|
+    File.open(filename, File::RDWR) do |file|
+      line 
+      p line.gsub!("AndroidIntelliJStarter", "AndroidNapikins")
+    end
+  end
+end
+
+File.open("readfile.rb", "r") do |infile|
+    while (line = infile.gets)
+      puts "#{counter}: #{line}"
+        counter = counter + 1
+    end
+end
