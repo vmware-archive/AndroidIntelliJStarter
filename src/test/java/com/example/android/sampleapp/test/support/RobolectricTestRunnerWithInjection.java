96869d20811dd723c4c3399b196a632ee618e55c
Phil Goodwin <pair+phil@pivotallabs.com>
2013-03-20 13:39:48 -0700

    Upgrading to Maven generated build file. Fixed problem with RoboGuice integration.

--- src/test/java/com/example/android/sampleapp/test/support/RobolectricTestRunnerWithInjection.java
+++ src/test/java/com/example/android/sampleapp/test/support/RobolectricTestRunnerWithInjection.java
@@ -5,8 +5,10 @@ import com.example.android.sampleapp.MySampleApplication;
 import com.example.android.sampleapp.util.CurrentTime;
 import org.junit.runners.model.InitializationError;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.DefaultTestLifecycle;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.internal.TestLifecycle;
 import roboguice.RoboGuice;
 
 public class RobolectricTestRunnerWithInjection extends RobolectricTestRunner {
@@ -16,16 +18,10 @@ public class RobolectricTestRunnerWithInjection extends RobolectricTestRunner {
     }
 
     @Override
-    public void prepareTest(Object test) {
-        MySampleApplication application = (MySampleApplication) Robolectric.application;
-
-
-        RoboGuice.setBaseApplicationInjector(application, RoboGuice.DEFAULT_STAGE,
-                RoboGuice.newDefaultRoboModule(application), new TestApplicationModule());
-
-        RoboGuice.getInjector(application).injectMembers(test);
-        MockitoAnnotations.initMocks(test);
+    protected Class<? extends TestLifecycle> getTestLifecycleClass() {
+        return TestLifeCycleWithInjection.class;
     }
+
 
     public static class TestApplicationModule extends ApplicationModule {
         @Override
@@ -36,4 +32,17 @@ public class RobolectricTestRunnerWithInjection extends RobolectricTestRunner {
         }
     }
 
+    public static class TestLifeCycleWithInjection extends DefaultTestLifecycle {
+        @Override
+        public void prepareTest(Object test) {
+            MySampleApplication application = (MySampleApplication) Robolectric.application;
+
+
+            RoboGuice.setBaseApplicationInjector(application, RoboGuice.DEFAULT_STAGE,
+                    RoboGuice.newDefaultRoboModule(application), new TestApplicationModule());
+
+            RoboGuice.getInjector(application).injectMembers(test);
+            MockitoAnnotations.initMocks(test);
+        }
+    }
 }
